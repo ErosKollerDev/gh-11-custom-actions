53d72c04f2fcbcfe1ed135281fb3573a2d50ebde
eros <eroskoller@gmail.com>
2025-01-14 18:47:58 -0300

    Adding screts to env

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -5,6 +5,16 @@ const github = require('@actions/github')
 function run() {
     core.info('Deploying to S3');
     core.notice('Starting process to deploy to S3');
+    // 1) Get inputs
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    // 2) Upload to S3
+    exec.exec(`aws s3 sync ${distFolder} s3://${bucket} --region ${bucketRegion}`);
+
+
+
 }
 
 
